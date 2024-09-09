8fb7580bc60fa43c4b841c265812cc27027f94b5
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-09-09 23:14:40 +0800

    feat: create comment module

---
+++ src/app/modules/comments/DTO/comment.ts
@@ -0,0 +1,7 @@
+export class PostDto {
+  id: number;
+  created_at: Date;
+  poster_id: number;
+  post_id: number;
+  body: string;
+}
---
+++ src/app/modules/comments/services/comment.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { Supabase } from 'src/db/supabase.service';
+
+@Injectable()
+export class CommentsService {
+  constructor(private readonly supabase: Supabase) {}
+  private readonly cats = [];
+
+  async getAllPosts() {
+    const posts = await this.supabase
+      .getClient()
+      .from('posts')
+      .select(
+        `
+      id, 
+      title, 
+      body,
+      created_at,
+      users ( id, username, profile_pic_url )
+    `,
+      )
+      .order('id', { ascending: false });
+
+    return posts;
+  }
+
+  async getPostById(post_id: number) {
+    const post = await this.supabase
+      .getClient()
+      .from('posts')
+      .select(
+        `
+      id, 
+      title, 
+      body,
+      created_at,
+      users ( id, username, profile_pic_url )
+    `,
+      )
+      .eq('id', post_id)
+      .order('id', { ascending: false });
+
+    return post;
+  }
+
+  async createPost(body: string, title: string, user_id: number) {
+    await this.supabase
+      .getClient()
+      .from('posts')
+      .insert([{ body, title, user_id }]);
+
+    return await this.supabase
+      .getClient()
+      .from('posts')
+      .select(
+        `
+    id, 
+    title, 
+    body,
+    created_at,
+    users ( id, username, profile_pic_url )
+  `,
+      )
+      .order('id', { ascending: false });
+  }
+}
